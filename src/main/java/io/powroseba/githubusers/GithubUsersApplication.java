7beca09237781a9067ed28cec110d67b25e49b9f
Sebastian Powroźnik <sebastian.powroznik@allegro.pl>
2023-07-07 15:47:33 +0200

    E-202307071547 - initial commit

---
+++ src/main/java/io/powroseba/githubusers/GithubUsersApplication.java
@@ -0,0 +1,13 @@
+package io.powroseba.githubusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubUsersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/powroseba/githubusers/GithubUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.powroseba.githubusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
