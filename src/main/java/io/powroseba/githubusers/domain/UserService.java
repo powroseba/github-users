4f7b3e819d7f7fcb8bbcee6309cce3456e9415bd
Sebastian Powroźnik <sebastian.powroznik@allegro.pl>
2023-07-09 13:58:11 +0200

    E-202307081357 - refactoring attempt counter decorator to use composition over inheritance

--- src/main/java/io/powroseba/githubusers/domain/UserService.java
+++ src/main/java/io/powroseba/githubusers/domain/UserService.java
@@ -1,33 +1,8 @@
 package io.powroseba.githubusers.domain;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.Optional;
 
-public class UserService {
+public interface UserService {
 
-    private static final Logger log = LoggerFactory.getLogger(UserService.class);
-    private final UserProvider userProvider;
-
-    public UserService(UserProvider userProvider) {
-        this.userProvider = userProvider;
-    }
-
-    public Optional<UserWithCalculations> get(Login login) {
-        log.info("Fetching user with login {}", login.value());
-        return userProvider.get(login)
-                .map(this::addCalculations);
-    }
-
-    private UserWithCalculations addCalculations(User user) {
-        return user.withCalculations(calculation(user));
-    }
-
-    private Double calculation(User user) {
-        if (user.followersCount().equals(0L)) {
-            return 0d;
-        }
-        return 6 / user.followersCount().doubleValue() * (2 + user.publicRepositoriesCount().doubleValue());
-    }
+    Optional<UserWithCalculations> get(Login login);
 }
--- src/main/java/io/powroseba/githubusers/infrastructure/datasource/AttemptCounterUserServiceDecorator.java
+++ src/main/java/io/powroseba/githubusers/infrastructure/datasource/AttemptCounterUserServiceDecorator.java
@@ -1,32 +1,34 @@
 package io.powroseba.githubusers.infrastructure.datasource;
 
 import io.powroseba.githubusers.domain.Login;
-import io.powroseba.githubusers.domain.UserProvider;
 import io.powroseba.githubusers.domain.UserService;
 import io.powroseba.githubusers.domain.UserWithCalculations;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Primary
 @Service
-class AttemptCounterUserServiceDecorator extends UserService {
+class AttemptCounterUserServiceDecorator implements UserService {
 
     private static final String UPDATE_COUNTER = "UPDATE LOGIN_REQUESTS set request_count = request_count + 1 WHERE login = ?";
     private static final String INSERT_COUNTER = "INSERT INTO LOGIN_REQUESTS (login, request_count) VALUES (?, 1)";
 
+    private final UserService userService;
     private final JdbcTemplate jdbcTemplate;
 
-    public AttemptCounterUserServiceDecorator(UserProvider userProvider, JdbcTemplate jdbcTemplate) {
-        super(userProvider);
+    public AttemptCounterUserServiceDecorator(UserService userService, JdbcTemplate jdbcTemplate) {
+        this.userService = userService;
         this.jdbcTemplate = jdbcTemplate;
     }
 
     @Transactional
     @Override
     public Optional<UserWithCalculations> get(Login login) {
-        var result = super.get(login);
+        var result = userService.get(login);
         increaseCounter(login);
         return result;
     }
